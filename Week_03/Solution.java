de37c389556b1d840bbcfe6891ddb218d71cd81c
ningjiaqi <ning_jq@suixingpay.com>
2020-07-12 23:09:35 +0800

    week03

---
+++ Week_03/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: ning_jq[ning_jq@suixingpay.com]
+ * @date: 2020/7/12 下午11:04
+ * @Copyright ©2020 Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+package designPatterns.strategy;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    private TreeNode ans;
+
+    public Solution() {
+        this.ans = null;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return false;
+        }
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if ((lson && rson) || ((root.val == p.val || root.val == q.val) && (lson || rson))) {
+            ans = root;
+        }
+        return lson || rson || (root.val == p.val || root.val == q.val);
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        this.dfs(root, p, q);
+        return this.ans;
+    }
+}
---
+++ Week_03/Strategy.java
@@ -0,0 +1,23 @@
+/**
+ * All rights Reserved, Designed By Suixingpay.
+ *
+ * @author: ning_jq[ning_jq@suixingpay.com]
+ * @date: 2020/5/24 下午8:14
+ * @Copyright ©2020 Suixingpay. All rights reserved.
+ * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
+ */
+package designPatterns.strategy;
+
+/**
+ * 策略抽象
+ *
+ * @author: ning_jq[ning_jq@suixingpay.com]
+ * @date: 2020/5/24 下午8:14
+ * @version: V1.0
+ * @review: ning_jq[ning_jq@suixingpay.com]/2020/5/24 下午8:14
+ */
+public abstract class Strategy {
+
+    public abstract void algorithmInterface();
+
+}
