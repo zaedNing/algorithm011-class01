7f25415fe48e3ce49834222dd8ac579b98467c76
ningjiaqi <ning_jq@suixingpay.com>
2020-08-02 20:32:53 +0800

    week06

--- Week_03/Strategy.java
+++ Week_03/Strategy.java
@@ -16,8 +16,36 @@ package designPatterns.strategy;
  * @version: V1.0
  * @review: ning_jq[ning_jq@suixingpay.com]/2020/5/24 下午8:14
  */
-public abstract class Strategy {
+public class Strategy {
 
-    public abstract void algorithmInterface();
+    public void backtrack(int n,
+                        ArrayList<Integer> output,
+                        List<List<Integer>> res,
+                        int first) {
+    // 所有数都填完了
+    if (first == n)
+      res.add(new ArrayList<Integer>(output));
+    for (int i = first; i < n; i++) {
+      // 动态维护数组
+      Collections.swap(output, first, i);
+      // 继续递归填下一个数
+      backtrack(n, output, res, first + 1);
+      // 撤销操作
+      Collections.swap(output, first, i);
+    }
+  }
+
+  public List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new LinkedList();
+
+    ArrayList<Integer> output = new ArrayList<Integer>();
+    for (int num : nums)
+      output.add(num);
+
+    int n = nums.length;
+    backtrack(n, output, res, 0);
+    return res;
+  }
+
 
 }
---
+++ Week_06/Balloon.java
@@ -0,0 +1,24 @@
+
+public class Balloon {
+
+	    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[][] rec = new int[n + 2][n + 2];
+        int[] val = new int[n + 2];
+        val[0] = val[n + 1] = 1;
+        for (int i = 1; i <= n; i++) {
+            val[i] = nums[i - 1];
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j <= n + 1; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    int sum = val[i] * val[k] * val[j];
+                    sum += rec[i][k] + rec[k][j];
+                    rec[i][j] = Math.max(rec[i][j], sum);
+                }
+            }
+        }
+        return rec[0][n + 1];
+    }
+
+}
